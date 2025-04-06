c5787d636544cda7df542fbdba8830fcf8c4fdfe
dFaery <137483979+dFaery@users.noreply.github.com>
2025-04-06 19:37:57 +0700

    added conditional statement at setting.php to display coolies value at input area

--- setting.php
+++ setting.php
@@ -19,17 +19,30 @@ if (isset($_POST['Simpan'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Setting</title>
 </head>
+
 <body>
     <h1>Halaman Setting</h1>
     <form action="setting.php" method="POST">
         <!-- Text-Align -->
         <label for="text-align">Text-Align:</label>
         <select name="text-align" id="text-align" required>
-            <option value="">-- Pilih Text-align --</option>
+            <option value="">
+
+                <!-- CHECK: if text-align cookie has been created, display text-align cookie to combobox -->
+                <?php
+                if (isset($_COOKIE['text-align']))
+                    echo $_COOKIE['text-align'];
+                else {
+                    echo "-- Pilih Text-align --";
+                }
+                ?>
+
+            </option>
             <option value="left">Left</option>
             <option value="right">Right</option>
             <option value="center">Center</option>
@@ -40,7 +53,18 @@ if (isset($_POST['Simpan'])) {
         <!-- Font-Family -->
         <label for="font-family">Font-Family:</label>
         <select name="font-family" id="font-family" required>
-            <option value="">-- Pilih Font --</option>
+            <option value="">
+
+                <!-- CHECK: if font-family cookie has been created, display font-family cookie to combobox -->
+                <?php
+                if (isset($_COOKIE['font-family']))
+                    echo $_COOKIE['font-family'];
+                else {
+                    echo "-- Pilih font-family --";
+                }
+                ?>
+
+            </option>
             <option value="Arial">Arial</option>
             <option value="Tahoma">Tahoma</option>
             <option value="Calibri">Calibri</option>
@@ -49,10 +73,20 @@ if (isset($_POST['Simpan'])) {
 
         <!-- Color -->
         <label for="color">Color:</label>
-        <input type="color" name="color" id="color" value="#000000" required>
+        <input type="color" name="color" id="color" value="
+        <?php
+
+        // CHECK: if color cookie has been created, display color to input:color
+        if (isset($_COOKIE['color'])) {
+            echo $_COOKIE['color'];
+        } else {
+            echo "#000000";
+        }
+        ?>" required>
         <br><br>
 
         <input type="submit" name="Simpan" value="Simpan">
     </form>
 </body>
+
 </html>
