f55145903e74bcbee349c020ba4b71ae6b960101
clath1a <alethiaclarita1@gmail.com>
2025-06-03 08:31:23 +0700

    [DONE] index.php

--- UAS/index.php
+++ UAS/index.php
@@ -4,17 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UAS WEBPROG - ILO MATES</title>
+
+    <!-- Custom CSS & Fonts -->
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rubik+One&display=swap" rel="stylesheet">
 </head>
 <body>
-    <form action="">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required>
+    <form action="order.php"  method="POST">
+        <label for="kode">Kode Makanan:</label>
+        <input type="text" id="KodeMakanan" name="KodeMakanan" required>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
+        <label for="makanan">Makanan:</label>
+        <input type="text" id="Makanan" name="Makanan" required>
 
-        <label for="message">Message:</label>
-        <textarea id="message" name="message" required></textarea>
+        <label for="harga">Harga Makanan:</label>
+        <input type="text" id="harga" name="harga" required>
+
+        <label for="foto">Foto Makanan:</label>
+        <input type="file" id="foto" name="foto" accept="image/jpeg, image/png" require>
 
         <button type="submit">Submit</button>
     </form>
--- UAS/order.php
+++ UAS/order.php
@@ -1,11 +1,25 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PROSES UAS WEBPROG - ILO MATES</title>
+
+    <!-- Custom CSS & Fonts -->
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rubik+One&display=swap" rel="stylesheet">
 </head>
 <body>
-    
+    <h1>Detail Pesanan</h1>
+    <p><strong>Kode Makanan:</strong> <?php echo $_SESSION['KodeMakanan']; ?></p>
+    <p><strong>Makanan:</strong> <?php echo $_SESSION['Makanan']; ?></p>
+    <p><strong>Harga:</strong> <?php echo $_SESSION['harga']; ?></p>
+    <p><strong>Foto:</strong> <img src="<?php echo $_SESSION['foto']; ?>" alt="Foto Makanan"></p>
+</body>
+</html>
+
 </body>
 </html>
